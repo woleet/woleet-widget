e75d1b7c9d2e49d05e8a80e3758fce45502fa6bb
vbarat <vincent.barat@woleet.com>
2020-06-19 13:41:16 +0200

    woleet-weblibs 0.9.2 used. Fix minification.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 const gulp = require("gulp"),
-    less = require('gulp-less'),
-    babel = require('gulp-babel'),
-    minify = require('gulp-minify'),
-    sourcemaps = require('gulp-sourcemaps');
+  less = require('gulp-less'),
+  babel = require('gulp-babel'),
+  minify = require('gulp-minify'),
+  sourcemaps = require('gulp-sourcemaps');
 
 // Generate Widget style
 gulp.task('less', function () {
-    return gulp.src('./res/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./dist'));
+  return gulp.src('./res/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist'));
 });
 
 // Uglify Widget source code
 gulp.task("widget", () => {
-    return gulp.src(['src/woleet-widget.js'])
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(gulp.dest("./dist/"))
-        .pipe(minify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("./dist/"))
+  return gulp.src(['src/woleet-widget.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(gulp.dest("./dist/"))
+    .pipe(minify({ ext: { min: '.min.js' } }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("./dist/"))
 });
 
 gulp.task('default', gulp.parallel('widget', 'less'));
